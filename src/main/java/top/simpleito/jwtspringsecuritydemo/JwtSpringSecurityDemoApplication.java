42eea930673604bd3906b576ea4d21a2f1b83fd3
zhouxj <zhouxj@singularitychina.com>
2020-01-15 18:06:21 +0800

    update

--- src/main/java/top/simpleito/jwtspringsecuritydemo/JwtSpringSecurityDemoApplication.java
+++ src/main/java/top/simpleito/jwtspringsecuritydemo/JwtSpringSecurityDemoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+/**
+ * 入口配置DemoWebSecurityConfigurer
+ */
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class JwtSpringSecurityDemoApplication {
 
